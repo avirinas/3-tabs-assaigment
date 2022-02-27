8d873b02a1c1295dc9681d4643d91fba387277dc
avirinas <arnasv231@gmail.com>
2022-02-27 23:09:35 +0200

    init

---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict"
+   function changeHeader(event, name) {
+       var tabs = document.querySelectorAll(".tab");
+       var buttons = document.querySelectorAll("button");
+
+       for (var i =0; i < tabs.length; i++){
+           tabs[i].style.display = "none";
+           buttons[i].classList.remove("selected");
+       }
+       
+       document.getElementById(name).style.display = "block";
+       event.currentTarget.classList.add("selected");
+
+       setTimeout(function(){
+           var tabs = document.querySelectorAll(".tab");
+           for(var x of tabs){
+               x.classList.remove("tabAnimation");
+           }
+           document.getElementById(name).classList.add("tabAnimation");
+   },100);
+   }
+   changeHeader("pirmas");
